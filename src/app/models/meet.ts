80f517aa376ef935c40ab910c737f1d5d2692bbf
dev-ditis <ditis-desenvolvimento@guarapuava.pr.gov.br>
2025-07-15 09:20:00 -0300

    Add files via upload

--- src/app/models/meet.ts
+++ src/app/models/meet.ts
@@ -1,3 +1,60 @@
 export default class Meet {
+    public readonly id: number ;
+    private title: string ;
+    private meetDate: Date ;
+    private startTime: Date ;
+    private endTime: Date ;
+    private roomId : string ;
     
+    public constructor (
+        id : number ,
+        title : string , 
+        meetDate : Date , 
+        startTime : Date , 
+        endTime : Date ,
+        roomId :string ,
+        ){
+        this.id = id ;
+        this.title = title;
+        this.meetDate = meetDate ;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.roomId = roomId;
+    }
+
+
+    // Getters
+    public getId(): number {
+        return this.id;
+    }
+    public getTitle(): string {
+        return this.title;
+    }
+    public getMeetDate(): Date {
+        return this.meetDate;
+    }
+    public getStartTime(): Date {
+        return this.startTime;
+    }
+    public getEndTime(): Date {
+        return this.endTime;
+    }
+    public getRoomId():string{
+        return this.roomId;
+    }
+
+
+    // Setters
+    public setTitle(title: string): void {
+        this.title = title;
+    }
+    public setMeetDate(date: Date): void {
+        this.meetDate = date;
+    }
+    public setStartTime(startTime: Date): void {
+        this.startTime = startTime;
+    }
+    public setEndTime(endTime: Date): void {
+        this.endTime = endTime;
+    }
 }
--- src/app/models/room.ts
+++ src/app/models/room.ts
@@ -1,7 +1,47 @@
-export default class Room {
-    private id!: number;
+export default class Room{
+    // Abstração da sala
+    public readonly id : number;
+    private nameRoom : string;
+    private capacity : number;
 
-    public getId(){
+    // Abstração de usuarios na sala
+    private presentUsers? : number;
+
+    public constructor (
+        id : number ,
+        nameRoom : string ,
+        capacity : number,
+        presentUsers:number
+    );
+
+    public constructor (
+        id : number ,
+        nameRoom : string ,
+        capacity : number,
+        presentUsers? :number
+        ){
+        this.id = id;
+        this.nameRoom = nameRoom;
+        this.capacity = capacity;
+        this.presentUsers = presentUsers;
+    }
+
+    // Getters
+    public getid(): number {
         return this.id;
+    }
+    public getNameRoom(): string {
+        return this.nameRoom;
+    }
+    public getCapacity(): number {
+        return this.capacity;
+    }
+
+    // Setters
+    public setNameRoom(nameRoom: string): void {
+        this.nameRoom = nameRoom;
+    }
+    public setCapacity(capacity: number): void {
+        this.capacity = capacity;
     }
 }
